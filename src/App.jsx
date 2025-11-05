c2eb752aa5b9fe9496ca86c9971db51acb6f037c
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2025-11-05 17:57:49 -0300

    Ajuste Arquitetura

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Login from './pages/Login';
-import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 import './App.css';
 
 function App() {
